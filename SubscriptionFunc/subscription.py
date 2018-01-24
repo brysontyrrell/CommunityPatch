016206b5ae433827c65fdde93479d7eb7fe0de22
Bryson Tyrrell <bryson.tyrrell@jamf.com>
2018-01-24 00:54:10 -0600

    Added subscription function
    New endpoint (/subscribe) allows a POST of a JSON object containing the
    ‘id’ and ‘json_url’ or a patch definition. Upon a successful pull, it
    will be saved to a DynamoDB table and the definition to S3 under the
    given ‘id’.
    
    Required third party modules are embedded with the function.

---
+++ SubscriptionFunc/subscription.py
@@ -0,0 +1,61 @@
+import json
+import os
+
+import boto3
+from botocore.exceptions import ClientError
+import requests
+
+dynamodb = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
+s3_bucket = boto3.resource('s3').Bucket(os.environ['S3_BUCKET'])
+
+
+def response(message, status_code):
+    print(message)
+    return {
+        'isBase64Encoded': False,
+        'statusCode': status_code,
+        'body': json.dumps(message),
+        'headers': {'Content-Type': 'application/json'}
+    }
+
+
+def initial_subscription_request(subscription):
+    resp = requests.get(subscription['json_url'], headers={'Accept': 'application/json'})
+
+    try:
+        resp.raise_for_status()
+        resp.json()
+    except requests.HTTPError as error:
+        return response({'error': error}, 400)
+    except json.JSONDecodeError:
+        return response({'error': f'Bad Request: URL did not return JSON content'}, 400)
+
+    try:
+        dynamodb.put_item(
+            Item={
+                'id': subscription['id'],
+                'json_url': subscription['json_url']
+            }
+        )
+    except ClientError as error:
+        return response({'error': f'Internal Server Error: {error}'}, 500)
+
+    try:
+        s3_bucket.put_object(Body=resp.text.encode(), Key=f"{subscription['id']}.json")
+    except ClientError as error:
+        return response({'error': f'Internal Server Error: {error}'}, 500)
+
+    return  response(
+        {'success': f"PatchServer has subscribed to title '{subscription['id']}' at {subscription['json_url']}"}, 201)
+
+
+def lambda_handler(event, context):
+    if not event['headers'].get('Content-Type') == 'application/json':
+        return response({'error': "Unsupported Media Type: must be 'application/json'"}, 415)
+
+    body = json.loads(event['body'])
+    print(body)
+    if not all(k in body for k in ('id', 'json_url')):
+        return response({'error': "Bad Request: 'id' and 'json_url' keys are required"}, 400)
+
+    return initial_subscription_request(body)
