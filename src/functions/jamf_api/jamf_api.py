78cf221f3bcd18a1333947b775db7b23c953d46f
Bryson Tyrrell <bryson.tyrrell@jamf.com>
2018-03-21 16:01:54 -0500

    /jamf/v1/software working

--- src/functions/jamf_api/jamf_api.py
+++ src/functions/jamf_api/jamf_api.py
@@ -1,119 +1,84 @@
 import json
+import logging
 import os
-import shutil
-import tempfile
 
 import boto3
 from botocore.exceptions import ClientError
 
-s3_bucket = boto3.resource('s3').Bucket(os.environ['S3_BUCKET'])
-tempdir = ''
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-# Downloading files from S3 is time consuming. ~40 patch definitions that need
-# to be loaded range from 4-5 seconds for the entire request. There are a few
-# options for bringing this response time down:
-#
-#  - Increase function memory: 128->256 cuts time down by nearly %50, but
-#    will consume more GB-seconds.
-#  - Implement parallel downloading: local processing of files is quick. The
-#    longest execution comes from downloading from S3. Writing in parallel
-#    downloads will yield faster responses.
-#  - Drop S3, move definitions to DynamoDB: the nuclear solution, but will
-#    likely yield the fastest speeds.
+dynamodb = boto3.resource('dynamodb').Table(os.getenv('DEFINITIONS_TABLE'))
+s3_bucket = boto3.resource('s3').Bucket(os.getenv('DEFINITIONS_BUCKET'))
 
 
-def response(message, status_code):
-    print(message)
-    shutil.rmtree(tempdir)
+def response(body_data, status_code):
+    """Returns a dictionary object for an API Gateway Lambda integration
+    response.
+
+    :param body_data: Content for JSON body of response
+    :type body_data: str or dict or list
+
+    :param int status_code: HTTP status code of response
+
+    :rtype: dict
+    """
+    if isinstance(body_data, str):
+        body = json.dumps({'message': body_data})
+    else:
+        body = json.dumps(body_data)
+
     return {
         'isBase64Encoded': False,
         'statusCode': status_code,
-        'body': json.dumps(message),
+        'body': body,
         'headers': {'Content-Type': 'application/json'}
     }
 
 
-def title_list():
-    titles = os.listdir(tempdir)
-    print(f'Temp Dir Contents: {titles}')
-    software_titles = list()
-
-    for title in titles:
-        with open(os.path.join(tempdir, title), 'r') as f_obj:
-            data = json.load(f_obj)
-
-            software_titles.append(
-                {
-                    'name': data['name'],
-                    'publisher': data['publisher'],
-                    'lastModified': data['lastModified'],
-                    'currentVersion': data['currentVersion'],
-                    'id': data['id']
-                }
-            )
-
-    return software_titles
-
-
-def software():
-    for title in s3_bucket.objects.all():
-        try:
-            path = os.path.join(tempdir, title.key)
-            s3_bucket.download_file(title.key, path)
-        except ClientError:
-            return response(
-                {'error': f'Internal Server Error: '
-                          f'Unable to load data for: {title.key}'},
-                500
-            )
-
-    return response(title_list(), 200)
-
-
-def select_software(titles):
-    print(f"Selected software titles requested: {', '.join(titles)}")
-    print(titles)
-    for title in s3_bucket.objects.all():
-        title_name = title.key.split('.')[0]
-        print(f'Title Name: {title_name}')
-        if title.key.split('.')[0] in titles:
-            print('Matched!')
-            try:
-                path = os.path.join(tempdir, title.key)
-                s3_bucket.download_file(title.key, path)
-            except ClientError:
-                return response(
-                    {'error': f'Title Not Found: {title_name}'}, 404)
-
-    return response(title_list(), 200)
-
-
-def patch_title(title):
-    path = os.path.join(tempdir, title)
-
+def list_software_titles():
     try:
-        s3_bucket.download_file(f'{title}.json', path)
-    except ClientError:
-        return response({'error': f'Title Not Found: {title}'}, 404)
+        titles = dynamodb.scan()
+    except ClientError as error:
+        logger.exception(f'DynamoDB: {error.response}')
+        return response(f'Internal Server Error: {error}', 500)
 
-    with open(path, 'r') as f_obj:
-        data = json.load(f_obj)
-
+    data = [item['title_summary'] for item in titles['Items']]
     return response(data, 200)
 
 
-def lambda_handler(event, context):
-    global tempdir
-    tempdir = tempfile.mkdtemp()
+# def patch_title(title):
+#     path = os.path.join(tempdir, title)
+#
+#     try:
+#         s3_bucket.download_file(f'{title}.json', path)
+#     except ClientError:
+#         return response({'error': f'Title Not Found: {title}'}, 404)
+#
+#     with open(path, 'r') as f_obj:
+#         data = json.load(f_obj)
+#
+#     return response(data, 200)
 
+
+def lambda_handler(event, context):
+    logger.info(event)
+    path = event['path']
     resource = event['resource']
     parameter = event['pathParameters']
 
+    logger.info(f'Generating response for {path}')
+
     if resource == '/jamf/v1/software':
-        return software()
+        return list_software_titles()
 
-    elif resource == '/jamf/v1/software/{proxy+}' and parameter:
-        return select_software(parameter['proxy'].split(','))
+    elif resource == '/jamf/v1/software/{proxy+}':
+        # parameter['proxy'].split(',')
+        return response('Placeholder', 200)
 
-    elif resource == '/jamf/v1/patch/{proxy+}' and parameter:
-        return patch_title(parameter['proxy'])
+    elif resource == '/jamf/v1/patch/{proxy+}':
+        # parameter['proxy']
+        return response('Placeholder', 200)
+
+    else:
+        return response('Not Found', 404)
