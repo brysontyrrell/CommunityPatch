21bd29fa6792ae44906cf7dbad6c136a6c2945b4
Bryson Tyrrell <bryson.tyrrell@gmail.com>
2019-11-07 21:12:33 -0600

    Move us-east-2 specific items to separate backend stack

--- backend/src/contributor_stream/index.py
+++ backend/src/contributor_stream/index.py
@@ -26,42 +26,45 @@ def lambda_handler(event, context):
     fernet = get_fernet()
 
     for record in event["Records"]:
-        data = dict()
-        data.update(parse_stream(record["dynamodb"]["Keys"]))
 
         if record["eventName"] == "INSERT":
-            data.update(parse_stream(record["dynamodb"]["NewImage"]))
-
+            # INSERT only occurs on new registrations
+            new_image = process_record_image(record)
+            decrypted_email = fernet.decrypt(new_image["email"]).decode()
             verification_url = urlunparse(
                 (
                     "https",
                     DOMAIN_NAME,
                     "api/v1/contributors/verify",
                     None,
-                    urlencode({"id": data["id"], "code": data["verification_code"]}),
+                    urlencode(
+                        {"id": new_image["id"], "code": new_image["verification_code"]}
+                    ),
                     None,
                 )
             )
+            send_email(decrypted_email, new_image["display_name"], verification_url)
 
-            try:
-                email_sns_client().publish(
-                    TopicArn=EMAIL_SERVICE_TOPIC,
-                    Message=json.dumps(
-                        {
-                            "recipient": fernet.decrypt(data["email"]).decode(),
-                            "message_type": "verification",
-                            "message_data": {
-                                "display_name": data["display_name"],
-                                "url": verification_url,
-                            },
-                        }
-                    ),
-                    MessageStructure="string",
-                )
-            except ClientError as error:
-                logger.exception(f"Error sending SNS notification: {error}")
+        elif record["eventName"] == "MODIFY":
+            new_image = process_record_image(record)
+            old_image = process_record_image(record, old=True)
+            # Determine the delta from the OldImage and discard AWS keys
+            delta = {
+                k: new_image[k]
+                for k in new_image
+                if k in old_image
+                and new_image[k] != old_image[k]
+                and not k.startswith("aws:")
+            }
 
     return "ok"
+
+
+def process_record_image(record, old=False):
+    data = dict()
+    data.update(parse_stream(record["dynamodb"]["Keys"]))
+    data.update(parse_stream(record["dynamodb"]["OldImage" if old else "NewImage"]))
+    return data
 
 
 def parse_stream(data):
@@ -103,3 +106,20 @@ def get_fernet():
 @lru_cache()
 def email_sns_client():
     return boto3.client("sns", region_name="us-east-2")
+
+
+def send_email(recipient, name, token):
+    try:
+        email_sns_client().publish(
+            TopicArn=EMAIL_SERVICE_TOPIC,
+            Message=json.dumps(
+                {
+                    "recipient": recipient,
+                    "message_type": "api_token",
+                    "message_data": {"display_name": name, "api_token": token},
+                }
+            ),
+            MessageStructure="string",
+        )
+    except ClientError as error:
+        logger.exception(f"Error sending SNS notification: {error}")
