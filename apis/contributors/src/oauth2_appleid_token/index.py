62586b4aa19517295b1f949bf14624378ac18267
Bryson Tyrrell <bryson.tyrrell@jamf.com>
2020-03-03 16:25:37 -0600

    Add implicit auth

--- apis/contributors/src/oauth2_appleid_token/index.py
+++ apis/contributors/src/oauth2_appleid_token/index.py
@@ -3,19 +3,26 @@ import os
 import requests
 
 CLIENT_ID = os.getenv("CLIENT_ID")
-DOMAIN_NAME = os.getenv('DOMAIN_NAME')
+DOMAIN_NAME = os.getenv("DOMAIN_NAME")
+
+session = requests.Session()
 
 
 def lambda_handler(event, context):
-    response = requests.post(
+    response = session.post(
         url=f"https://auth.{DOMAIN_NAME}/oauth2/token",
         headers={"Content-Type": "application/x-www-form-urlencoded"},
         data={
             "grant_type": "authorization_code",
             "client_id": CLIENT_ID,
-            "redirect_uri": f"https://contrinbutors.{DOMAIN_NAME}/oauth2/appleid/token",
-            "code": event["queryStringParameters"]["code"]
-        }
+            "redirect_uri": f"https://contributors.{DOMAIN_NAME}/oauth2/appleid/token",
+            "code": event["queryStringParameters"]["code"],
+        },
     )
 
-    return response.json()
+    return {
+        "isBase64Encoded": False,
+        "statusCode": response.status_code,
+        "body": response.text,
+        "headers": {"Content-Type": "application/json"},
+    }
